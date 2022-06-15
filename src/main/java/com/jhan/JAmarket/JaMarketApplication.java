23bd26bcb2eeccae95e54562ab967c8a627e8f22
JhanAntezana <jhan.antezana.y@uni.pe>
2022-06-15 17:32:26 -0500

    primer commit

---
+++ src/main/java/com/jhan/JAmarket/JaMarketApplication.java
@@ -0,0 +1,14 @@
+package com.jhan.JAmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaMarketApplication.class, args);
+		System.out.println("hola mundo, soy Jhan :hot");
+	}
+
+}
---
+++ src/test/java/com/jhan/JAmarket/JaMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhan.JAmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
