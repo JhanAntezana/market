f3a454adcca8502e6d431d862e1ca174bd782819
JhanAntezana <jhan.antezana.y@uni.pe>
2022-06-28 15:41:02 -0500

    implementacion de controlador de respuestas HTTP

--- src/main/java/com/jhan/JAmarket/web/controller/ProductController.java
+++ src/main/java/com/jhan/JAmarket/web/controller/ProductController.java
@@ -3,6 +3,8 @@ package com.jhan.JAmarket.web.controller;
 import com.jhan.JAmarket.domain.Product;
 import com.jhan.JAmarket.domain.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,28 +16,36 @@ public class ProductController {
     @Autowired
     private ProductService productService;
     @GetMapping("/all")
-    public List<Product> getAll(){
-        return productService.getAll();
+    public ResponseEntity<List<Product>> getAll(){
+        return new ResponseEntity<>(productService.getAll(), HttpStatus.OK);
     }
     @GetMapping("/{id}")
-    public Optional<Product> getProduct(@PathVariable("id") int productId){
-        return productService.getProduct(productId);
+    public ResponseEntity<Product> getProduct(@PathVariable("id") int productId){
+        return productService.getProduct(productId)
+                .map(prod -> new ResponseEntity<>(prod, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
     @GetMapping("/category/{categoryId}")
-    public Optional<List<Product>> getByCategory(@PathVariable("categoryId") int categoryId){
-        return productService.getByCategory(categoryId);
+    public ResponseEntity<List<Product>> getByCategory(@PathVariable("categoryId") int categoryId){
+        return productService.getByCategory(categoryId)
+                .map(products -> new ResponseEntity<>(products, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
     @GetMapping("/products/{price}")
-    public List<Product> getProductsFromPriceGreaterThan(@PathVariable("price") Float price){
-        return productService.getProductsFromPriceGreaterThan(price);
+    public ResponseEntity<List<Product>> getProductsFromPriceGreaterThan(@PathVariable("price") Float price){
+        return new ResponseEntity<>(productService.getProductsFromPriceGreaterThan(price),HttpStatus.OK);
     }
     @PostMapping("/save")
-    public Product save (@RequestBody Product product){
-        return productService.save(product);
+    public ResponseEntity<Product> save (@RequestBody Product product){
+        return new ResponseEntity<>(productService.save(product), HttpStatus.CREATED) ;
     }
     @DeleteMapping("/delete/{id}")
-    public boolean delete (@PathVariable("id") int productId){
-        return productService.delete(productId);
+    public ResponseEntity delete (@PathVariable("id") int productId){
+        if (productService.delete(productId)){
+            return new ResponseEntity(HttpStatus.OK);
+        }else{
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
     }
 
 }
